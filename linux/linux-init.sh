a25ad0e3ef411ae0c0d09cc32bbc40188d72c81e
v_chpshi <v_chpshi@tencent.com>
2023-01-16 16:08:24 +0800

    1.添加程序安装

--- linux/linux-init.sh
+++ linux/linux-init.sh
@@ -2,9 +2,11 @@
 
 #   定义nvm版本变量
 nvm_version=0.39.3
+#   定义当前目录路径
+current_dir=$(pwd)
 
 #   检查服务是否安装
-checkInstalled() {
+notInstalled() {
     if ! command -v "$1" >/dev/null 2>&1; then
         echo -e "\033[31m未安装$1\033[0m"
         return 0
@@ -14,43 +16,66 @@ checkInstalled() {
     return 1
 }
 
+#   检查文件是否存在
+checkExists() {
+    if [ -f "$1" ]; then
+        echo -e "\033[31m文件不存在:$1\033[0m"
+        return 0
+    else
+        return 1
+    fi
+}
+
 #   nvm检查和安装
-if checkInstalled nvm; then
+if notInstalled nvm; then
     echo -e '\033[32m正在安装nvm\033[0m'
     #   获取NVM并安装
     curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v${nvm_version}/install.sh" | bash
 fi
 
 #   node检查和安装
-if checkInstalled node; then
+if notInstalled node; then
     echo -e '\033[31m正在安装Node\033[0m'
     nvm install 16.19.0
 fi
 
 #   svn检查和安装
-if checkInstalled svn; then
+if notInstalled svn; then
     echo -e '\033[31m正在安装SVN\033[0m'
     sudo yum install -y subversion
 fi
 
 #   mysql检查和安装
-if checkInstalled mysql; then
+if notInstalled mysql; then
     echo -e '\033[31m正在安装Mysql\033[0m'
 fi
 
-#   minio检查和安装
-if checkInstalled minio; then
-    echo -e '\033[32m正在安装Minio\033[0m'
-    wget https://dl.min.io/server/minio/release/darwin-amd64/minio
-    chmod +x minio
-fi
+# #   minio检查和安装
+# if ! checkExists current_dir + './minio'; then
+#     echo -e '\033[32m正在安装Minio\033[0m'
+#     wget https://dl.min.io/server/minio/release/darwin-amd64/minio
+#     chmod +x minio
+# fi
 
 #   Gvm检查和安装
-if checkInstalled gvm; then
+if notInstalled gvm; then
+    echo -e '\033[32m正在安装Gvm\033[0m'
     bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 fi
 
+#   Pyenv检查和安装
+if notInstalled pyenv; then
+    echo -e '\033[32m正在安装Pyenv\033[0m'
+    curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+    #   添加环境变量和命令处理
+    {
+        echo "export PYENV_ROOT=\"$HOME/.pyenv\""
+        echo "export PATH=\"$PYENV_ROOT/bin:$PATH\""
+        echo "eval \"$(pyenv init -)\""
+    } >>~/.bash_profile
+fi
 # #   宝塔检查和安装
-# if echeckInstalled baota; then
+# if notInstalled baota; then
 #     wget -O install.sh http://download.bt.cn/install/install_6.0.sh && sh install.sh
 # fi
